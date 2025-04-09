31715cc7ad0cd173a9ef6f8d068334d8cf79e6de
Sefa Yılmaz <53448093+komandotayfa@users.noreply.github.com>
2025-04-10 02:18:43 +0300

    Added comments

--- home-backup.sh
+++ home-backup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+## Edit after -x to add or remove directories for zip to exclude
 zip -y -r HOME_BACKUP.zip ~/ -x ~/.local/share/vulkan\* ~/.local/share/umu\* ~/.cache\* ~/.steam\* ~/.local/share/Steam\* ~/.local/share/lutris\* ~/.local/share/net.lutris.Lutris\* ~/.local/share/Trash\* ~/Games\*
 
 echo "::: Finished Backing Up Your Home :::"
